df66dadaece42074c8b5ded2d6b4b1b963197ca0
AndreArjDev <andre.021arj@gmail.com>
2021-11-17 11:14:01 -0300

    2 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,27 +1,21 @@
 import { Component, Injectable, Input, Output } from '@angular/core';
 import { Cep } from '../ceps';
 
-
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class TopBarComponent {
+  @Input() ceps: Cep[] | undefined;
 
- @Input() ceps : Cep[] | undefined;
-
-public findAll(){
-  return this.ceps;
+  public findAll() {
+    return this.ceps;
+  }
 }
-
-}
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
