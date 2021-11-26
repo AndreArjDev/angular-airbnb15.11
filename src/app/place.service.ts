18a0a4b3eaba903194e31349ceeb37538686898b
gitpoet <87090335+gitpoet@users.noreply.github.com>
2021-11-26 16:21:11 -0300

    Update place.service.ts

--- src/app/place.service.ts
+++ src/app/place.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Place, Places } from './place';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class PlaceService {
@@ -13,5 +14,9 @@ export class PlaceService {
   }
   listPlace() {
     return this.http.get<Array<Place>>(this.url);
+  }
+
+  savePlace(place:Place): Observable<Place>{
+    return this.http.post<Place>(this.url, place);
   }
 }
